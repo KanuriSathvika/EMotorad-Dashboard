a1c73782fa255d1f5dc30beeea2eca75acb7aba1
KanuriSathvika <kanurisathvika310@gmail.com>
2024-11-06 23:49:28 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/EMotorad-Dashboard/",
+  
   plugins: [react()],
+  base:'/EMotorad-Dashboard/'
 })

835b6d96a3f6539648a0eab496c037eeaf93ab1a
KanuriSathvika <kanurisathvika310@gmail.com>
2024-11-06 23:41:31 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ import Home from './Components/Home/Home.jsx';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  // const [count, setCount] = useState(0)
+  
 
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Login/>}/>
       <Route path='/dashboard' element={<Home/>}/>
-      {/* </Route> */}
+    
         
     </Routes>
   </BrowserRouter>
